a59268e8b3117d465236052e60aed93bca514350
Sebastien Mondet <seb@mondet.org>
2022-10-27 15:16:26 -0400

    Apply `./please.sh lint`

--- raw.mli
+++ raw.mli
@@ -1,7 +1,7 @@
 (** Raw base58check conversions (using Digestif's SHA256 implementation). *)
 
-(** The type base58 encoded strings is an alias for [string]. *)
 type base58 = string
+(** The type base58 encoded strings is an alias for [string]. *)
 
 (** Convert strings from/to base58-encoded strings. *)
 module String : sig
@@ -9,6 +9,7 @@ module String : sig
   val of_base58 : base58 -> string
 
   module No_checksum : sig
-    val to_base58 : string -> base58 val of_base58 : base58 -> string
+    val to_base58 : string -> base58
+    val of_base58 : base58 -> string
   end
 end
